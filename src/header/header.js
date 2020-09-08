1963f7319ff44cfb563b1c7f9c255e3ee307d36e
Nitish <mrameshnitish@gmail.com>
2020-09-09 00:35:16 +0530

    header commit

--- src/header/header.js
+++ src/header/header.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem, Button } from 'reactstrap';
 import "./header.css"
+import {
+  Link
+} from "react-router-dom"
 
 const Header = (props) => {
   return (
     <div className="header">
       <Breadcrumb>
-        <BreadcrumbItem active><a href="/">Recipes</a></BreadcrumbItem>
+        <Link to="/"><BreadcrumbItem style={{color:"blue"}} active>Recipes</BreadcrumbItem></Link>
+        <BreadcrumbItem></BreadcrumbItem>
         <BreadcrumbItem>Ingredients</BreadcrumbItem>
       </Breadcrumb>
+      <a href="https://covidrtp.herokuapp.com/" target="_blank"><Button size="sm" className="covid">Covid Tracker</Button></a>
     </div>
   );
 };
